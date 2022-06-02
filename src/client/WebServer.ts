cf0247a8f02998369c02c005263355c77217a192
Luna <71079641+Luna-devv@users.noreply.github.com>
2022-06-02 19:51:01 +0200

    fix the jwt auth

--- src/client/WebServer.ts
+++ src/client/WebServer.ts
@@ -1,6 +1,8 @@
 import type { webServerOptions } from '../typings/webServer';
 import { Client } from '../index';
+
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 /**
  * The internal webserver manager for incoming webhook requests
@@ -29,33 +31,18 @@ export class WebServer {
      * webServer.registerPath('/');
      */
     async registerPath(path: string, client: Client) {
-        this.app.use(express.json());
+        this.app.use(express.text());
 
         this.app.post(path, (req: express.Request, res: express.Response) => {
-            if (req.headers.authorization !== this.authorization) {
+
+            let decoded;
+            try {
+                decoded = jwt.verify(req.body, this.authorization);
+            } catch (e) {
                 client.emit('invalidVoteRequest', req);
-                res.status(401).json({
-                    status: 401,
-                    message: 'Invalid authorization provided'
-                });
-                return;
             };
 
-            if (!req.body.bot || !req.body.user || !req.body.isTest) {
-                client.emit('invalidVoteRequest', req);
-                res.status(400).json({
-                    status: 400,
-                    message: 'Invalid voting body provided',
-                    __example: {
-                        bot: '857230367350063104',
-                        user: '821472922140803112',
-                        isTest: true
-                    }
-                })
-                return;
-            };
-
-            client.emit('vote', req.body);
+            client.emit('vote', decoded);
             res.status(200).json({
                 status: 200,
                 message: 'Successfully emited vote'
@@ -80,7 +67,7 @@ export class WebServer {
         this.app.use((req: express.Request, res: express.Response, next: any) => {
             res.setHeader('Access-Control-Allow-Origin', '*');
             res.setHeader('Access-Control-Allow-Methods', 'POST');
-            res.setHeader('Access-Control-Allow-Headers', 'Content-Type, authorization ');
+            res.setHeader('Access-Control-Allow-Headers', '*');
             res.setHeader('Access-Control-Allow-Credentials', 'true');
             next();
         });
